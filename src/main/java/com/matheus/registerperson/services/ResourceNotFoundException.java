bcefe3411ecb2fc8db6f65ce7acfac5ae9fbd6d1
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-03 00:55:45 -0300

    Create ResourceNotFoundException

---
+++ src/main/java/com/matheus/registerperson/services/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.matheus.registerperson.services;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+    public ResourceNotFoundException(String msg){
+        super(msg);
+    }
+}
