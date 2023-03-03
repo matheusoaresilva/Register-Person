5596d4d42774ee0b4a77d01fa0bdff2ee27f61c9
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-03 01:39:09 -0300

    Added @Transactional

--- src/main/java/com/matheus/registerperson/services/ClientService.java
+++ src/main/java/com/matheus/registerperson/services/ClientService.java
@@ -3,6 +3,7 @@ package com.matheus.registerperson.services;
 import com.matheus.registerperson.entities.Client;
 import com.matheus.registerperson.repositories.ClientRepository;
 import com.matheus.registerperson.services.exceptions.ResourceNotFoundException;
+import jakarta.transaction.Transactional;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -22,16 +23,19 @@ public class ClientService {
 
     private static final Logger logger = LogManager.getLogger(ClientService.class);
 
+    @Transactional
     public Client findById(Long id){
         Optional<Client> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ResourceNotFoundException("Client Not found with id: " + id));
     }
 
+    @Transactional
     public Client insert(Client client){
         Client savedClient = repository.save(client);
         return savedClient;
     }
 
+    @Transactional
     public Client update(Long id, Client client){
         Client existingClient = repository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Client not found with id: " + id));
@@ -45,6 +49,7 @@ public class ClientService {
         return updatedClient;
     }
 
+    @Transactional
     public void delete(Long id){
         try{
             repository.deleteById(id);
