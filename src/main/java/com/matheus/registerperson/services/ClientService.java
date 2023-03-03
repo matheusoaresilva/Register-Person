34e1bdf1be4e0d1281f4f6d7ac8effa2400c9556
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-03 01:01:07 -0300

    Implemented findbyId method

---
+++ src/main/java/com/matheus/registerperson/services/ClientService.java
@@ -0,0 +1,21 @@
+package com.matheus.registerperson.services;
+
+import com.matheus.registerperson.entities.Client;
+import com.matheus.registerperson.repositories.ClientRepository;
+import com.matheus.registerperson.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    ClientRepository repository;
+
+    public Client findById(Long id){
+        Optional<Client> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException("Client Not found with id: " + id));
+    }
+}
