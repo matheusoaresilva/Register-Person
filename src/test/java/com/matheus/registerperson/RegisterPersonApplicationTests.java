e50f66d812cbeaa963ef6e343f3e387482502f03
Matheus Soares <matheusmatias2622@gmail.com>
2023-03-02 23:29:22 -0300

    Created Project

---
+++ src/test/java/com/matheus/registerperson/RegisterPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.registerperson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
